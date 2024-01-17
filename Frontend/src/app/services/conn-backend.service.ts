afb4f68df42f13bb90706acab1c0e7b05ff59bbc
DiegoQuiF <diego.quinonez@unmsm.edu.pe>
2024-01-17 12:56:55 -0500

    v1.3.3 17/01/2024

--- Frontend/src/app/services/conn-backend.service.ts
+++ Frontend/src/app/services/conn-backend.service.ts
@@ -8,8 +8,8 @@ import { Usuario } from '../models/usuario';
 })
 export class ConnBackendService {
 
-  //private BASE_URL = 'https://paginatad01.onrender.com';
-  private BASE_URL = 'http://127.0.0.1:5000'
+  private BASE_URL = 'https://paginatad01.onrender.com';
+  //private BASE_URL = 'http://127.0.0.1:5000'
   constructor(private http:HttpClient) { }
 
   getUsuarios():Observable<any>{
