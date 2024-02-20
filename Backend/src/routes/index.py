82e5bc94ef8c8352d474b3367911b56592f2d7ef
Jul1o21 <julio.cubas@unmsm.edu.pe>
2024-02-20 02:45:32 -0500

    v2.5.0
    Agregada una nueva pestaña para el analisis de datos de la base de datos. Ir la pestaña de http://localhost:4200/datos

--- Backend/src/routes/index.py
+++ Backend/src/routes/index.py
@@ -486,7 +486,6 @@ def obtenerMaterialesPorAnio():
     except Exception as e:
         return jsonify({'message':'ERROR', 'success':False})
 
-
 @main.route('/getFacturasPorFecha')
 def obtenerFacturasPorFecha():
     try:
